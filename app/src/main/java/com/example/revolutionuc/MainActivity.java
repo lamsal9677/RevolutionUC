d7fe8a2bab2e656a1528033144671d9d9ed9e6cb
Sanskar Lamsal <lamsal9677@yahoo.com>
2020-02-22 16:16:05 -0500

    v1

--- app/src/main/java/com/example/revolutionuc/MainActivity.java
+++ app/src/main/java/com/example/revolutionuc/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.revolutionuc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //button id is button
+
+        Button buttonn = (Button) findViewById(R.id.button);
+        buttonn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
     }
-}
+        public void openActivity2(){
+            Intent intent = new Intent(this, openActivity2.class);
+            startActivity(intent);
+        }
+    }
+
